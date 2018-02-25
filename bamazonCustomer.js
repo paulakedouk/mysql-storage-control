5ddf1c53bada7410e7097d1c7867f23f39ce8d4f
paulakedouk <paulakedouk@gmail.com>
2018-02-24 21:03:31 -0800

    Try to add stock product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,58 +1,86 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table= require("cli-table");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  user: "root",
+    user: "root",
 
-  password: "password",
-  database: "bamazon"
+    password: "password",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
 
-  products();
+    products();
 });
 
 function products() {
-     connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         var table = new Table({
-            head: ['ID', 'Product','Department', 'Price','Stock Qtd'],
+            head: ['ID', 'Product', 'Department', 'Price', 'Stock Qtd'],
             colWidths: [15, 15, 15, 15, 15]
         });
 
         // table is an Array, so you can `push` , `unshift`, `splice` and friends
-        for( i = 0; i < res.length; i++){
+        for (i = 0; i < res.length; i++) {
             table.push(
                 [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-             );
+            );
         }
 
         console.log(table.toString());
-     })
 
-    //  inquirer
-    // .prompt({
-    //   type: "input",
-    //   name: "artist",
-    //   message: "What is the ID of the item you would like to purchase?"
-    // })
-    // .then(function(answer) {
-    //   var query = "SELECT * FROM products";
-    //   connection.query(query, function(err, res) {
-    //     if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "item",
+                    message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+                    validate: function (value) {
+                        if (value >= 0 && value <= 10) {
+                            return true;
+                        } else if (isNaN(value) === false) {
+                            return true;
+                        } else if (value == "Q") {
+                            console.log("\nThank you! Bye.")
+                            process.exit();
+                        } else {
+                            return 'Please enter a valid item ID!';
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'quantity',
+                    message: 'How many units of the product would you like? [Quit with Q]',
+                    validate: function (value) {
+                        if (isNaN(value)) {
+                            return 'Please enter a valid item ID!';
+                        } else {
+                            return true;
+                        }
+                    }
+
+                }
+
+            ])
+            .then(function (userInput) {
+                var query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
+                connection.query(query, { id: userInput.item }, function (err, res) {
+                    if (err) throw err;
+
+                    console.log('stock ' + res[0].stock_quantity)
+                    console.log('user qnt ' + userInput.quantity)
+                });
+            });
+    })
 
 
-    //     for (var i = 0; i < res.length; i++) {
-    //       console.log(res[i]);
-    //     }
-    //   });
-    // });
 }
